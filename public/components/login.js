768bfb700a32b83134e19c8628db9ed1c9b113e6
JamesQuillin <jamesquillin1@gmail.com>
2017-06-26 12:15:52 -0700

    Update component logic in components/login.js

---
+++ public/components/login.js
@@ -0,0 +1,8 @@
+angular.module('flash-card')
+.controller('LoginCtrl', function(){
+
+})
+.component('login', {
+  controller: 'LoginCtrl',
+  templateUrl: './templates/login.html' //calling from index.html
+});
