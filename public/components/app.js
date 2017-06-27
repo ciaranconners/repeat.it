77514c7b326e348dc40f4916bcb615dc611bb326
Sheena Ramirez <sheenaramirez2@gmail.com>
2017-06-27 13:17:47 -0500

    Add ability to grab data from http request

--- public/components/app.js
+++ public/components/app.js
@@ -1,9 +1,11 @@
 angular.module('flash-card')
-
 .controller('AppCtrl', function($http) {
-
+  var that = this;
+  $http.get('../data/data.json').then(function(res) {
+    that.data = res.data;
+  });
 })
 .component('app', {
   controller: 'AppCtrl',
-  templateUrl: './templates/app.html'
+  templateUrl: './templates/app.html',
 });
