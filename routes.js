e7af6fd823cc98c065c479a27fec068d21274800
JamesQuillin <jamesquillin1@gmail.com>
2017-07-01 10:52:35 -0700

    Fix bug with second+ decks for a user
    Victory

--- routes.js
+++ routes.js
@@ -37,15 +37,11 @@ router.post('/decks', function(req, res) {
 
 router.put('/decks/', function(req, res) {
   var username = req.body.username;
+  var deckname = req.body.deckname;
   console.log(username);
-  //console.log('USERNAME', username); => confirms that we have correct username serverside
-  console.log('REQ.BODY', req.body);
-  Deck.findOneAndUpdate({username: username}, req.body, {new:true}).then(function(err, deck) {
-    // res.json(deck);
-    // if (err) {
-    //   res.status(200).json('put failed');
-    // }
-    console.log('DECK', deck);
+  console.log(deckname);
+
+  Deck.findOneAndUpdate({username: username, deckname: deckname}, req.body, {new:true}).then(function(deck) {
     res.status(200).send('deck updated');
   });
 });
