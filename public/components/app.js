f4fcaaac8ba554fb0a2d42cda95e5b3e3e0cf4a3
JamesQuillin <jamesquillin1@gmail.com>
2017-07-01 16:10:24 -0700

    Attempt 2 to add ciarans changes

--- public/components/app.js
+++ public/components/app.js
@@ -1,23 +1,21 @@
 angular.module('flash-card')
-.controller('AppCtrl', function($http, $location, $route, $timeout) {
+.controller('AppCtrl', function($http, $timeout) {
   var that = this;
-
   var currentUser = localStorage.getItem('currentUser');
-
   this.setDecks = function() {
       that.decks = JSON.parse(localStorage.getItem('decks'));
       console.log('setDecks called. this.decks: ', that.decks);
   };
-
   this.getDeck = function(deck){
     localStorage.setItem('currentDeck', JSON.stringify(deck));
   };
-
   this.handleDelete = function(deck) {
     var id = deck._id;
     $http.delete('/decks/' + id).then(function() {
       $http.get('/decks', {params:{username: currentUser}}).then(function(res) {
+        localStorage.setItem('decks', JSON.stringify(res.data));
         that.decks = res.data;
+        console.log('inside handle delete', localStorage.getItem('decks'));
       }, function(error) {console.error(error);});
     }, function(error) {console.error(error);});
   };
--- public/components/editPage.js
+++ public/components/editPage.js
@@ -1,11 +1,8 @@
 angular.module('flash-card')
 .controller('EditPageCtrl', function($http, $location){
-
   this.newCard = {plaintextFront: true, plaintextBack: true};
-
   //***** add more of the default schema ****
   this.deck = JSON.parse(localStorage.getItem('currentDeck'));
-
   this.addCard = function(newCard) {
     if(!newCard.front || !newCard.back) {
       alert("Please fill out a card");
@@ -15,41 +12,24 @@ angular.module('flash-card')
     }
     console.log('this.deck----', this.deck);
   };
-
   this.handleSave = function() {
-
     if(!this.deck.deckname) {
       alert("Please enter a deck name");
     } else {
-
-      console.log('this.deck----(23)', this.deck);
       var id = this.deck._id;
-      console.log('id----(25)', id);
-      console.log('current user----(26)', localStorage.getItem('currentUser'));
-      console.log('this.deck.username (27)', this.deck.username);
-
       $http.put('/decks/', this.deck, {params: {username: localStorage.getItem('currentUser')}}).then(function() {
-
         $http.get('/decks', {params: {username: localStorage.getItem('currentUser')}}).then(function(response) {
-          console.log('current user----(31)', localStorage.getItem('currentUser'));
-
           console.log('getting decks', response);
           localStorage.setItem('decks', JSON.stringify(response.data));
           $location.path('/app');
         }, function(err) {console.error('handleSave, EDIT', err);});
       }, function(err) {console.error(err);});
-
-
-
     }
   };
-
-
   this.deleteCard = function(card) {
     var i = this.deck.cards.indexOf(card);
     this.deck.cards.splice(i,1);
   };
-
   this.moveUp = function(card) {
     var index = this.deck.cards.indexOf(card);
     if(index === 0) {
@@ -60,7 +40,6 @@ angular.module('flash-card')
       this.deck.cards[index] = temp;
     }
   };
-
   this.moveDown = function(card) {
     var index = this.deck.cards.indexOf(card);
     if(index === this.deck.cards.length-1) {
@@ -71,19 +50,14 @@ angular.module('flash-card')
       this.deck.cards[index] = temp;
     }
   };
-
   this.toggleHighlightFront = function(card) {
     card.plaintextFront = !card.plaintextFront;
   };
-
   this.toggleHighlightBack = function(card) {
     card.plaintextBack = !card.plaintextBack;
   };
-
 })
 .component('editPage', {
   controller: 'EditPageCtrl',
   templateUrl: './templates/editPage.html' //calling from index.html
 });
-
-
