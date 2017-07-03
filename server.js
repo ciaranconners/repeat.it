34ecc94aa1105c32a4612f26eb78ee13f41ea224
James Quillin <jamesquillin1@gmail.com>
2017-07-03 16:48:17 -0700

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ var mongoose = require('mongoose');
 //change name after we change the placeholder.js
 var db = require('./db/config');
 
+// see routes line 144 for description
 var specialHashRegex = '/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/';
 
 app.use(bodyParser.json());
