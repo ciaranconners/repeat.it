b538efdf4f3ac53bb743295ef43bdf9e8d51f0ff
JunjiOtsukaPca <jun531@gmail.com>
2017-07-02 22:22:49 -0700

    from edit page deck name can be changed

--- public/components/editPage.js
+++ public/components/editPage.js
@@ -12,6 +12,7 @@ angular.module('flash-card')
     }
     console.log('this.deck----', this.deck);
   };
+
   this.handleSave = function() {
     if(!this.deck.deckname) {
       alert("Please enter a deck name");
@@ -26,6 +27,26 @@ angular.module('flash-card')
       }, function(err) {console.error(err);});
     }
   };
+
+  this.handleEditAndSave = function() {
+    if(!this.deck.deckname) {
+      alert("Please enter a deck name");
+    } else {
+      var id = this.deck._id;
+      $http.put(`/decks/${id}`, this.deck)
+        .then(function() {
+          $http.get('/decks', {params: {username: localStorage.getItem('currentUser'),  deckname: JSON.parse(localStorage.getItem('currentDeck')).deckname}})
+            .then(function(response) {
+              console.log('getting decks', response);
+              localStorage.setItem('decks', JSON.stringify(response.data));
+              // $location.path('/app');
+            },
+          function(err) {console.error('handleSave, EDIT', err);});
+        },
+      function(err) {console.error(err);});
+    }
+  };
+
   this.deleteCard = function(card) {
     var i = this.deck.cards.indexOf(card);
     this.deck.cards.splice(i,1);
